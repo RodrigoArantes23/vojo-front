804a727374437cf369d9946795ac09ce401c59e3
unknown <rodrigo.arantes@outlook.com>
2021-01-31 21:08:08 -0300

    fix: remove comments

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -3,8 +3,6 @@ import { Route, Redirect } from "react-router-dom";
 import { isLoggedIn } from "../services/Auth";
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
-  // Show the component only when the user is logged in
-  // Otherwise, redirect the user to /signin page
   <Route
     {...rest}
     render={(props) =>
